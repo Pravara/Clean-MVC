9bfbd3f8a7bd804b7b9195e7cec8db9231d1a5f6
Pravara Kant <pkant@hotels.com>
2018-12-17 09:32:18 +0000

    sample project to demonstrate using MVC with coordinators

---
+++ FoodTracker1/FoodTracker/FoodTracker/RecipesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecipesViewController.swift
+//  FoodTracker
+//
+//  Created by Pravara Kant on 17/12/2018.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
